ebd099f68e12ad1199ffb921cf4ee7d0a6624b84
Admin <11265585+tianjin-polytechnic-university_0@user.noreply.gitee.com>
2022-09-27 10:43:31 +0800

    add header + pop view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <router-view></router-view>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <site-navigation></site-navigation>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
-
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
 <style scoped>
-
 </style>
